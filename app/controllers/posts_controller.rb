0cc7c39ba9c0e3cf694b10ae8dfcb1ed247b76c9
MuhammadBilal <bilalzafarr39@gmail.com>
2021-12-07 15:49:52 +0500

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,63 @@
+class PostsController < ApplicationController
+    # before_action :find_post, only: [:show, :update, :edit, :destroy]
+
+   def index
+       @posts = Post.all.order("created_at DESC")
+   end
+
+   def new
+       @post = Post.new
+   end
+
+   def create
+       @post = Post.new(post_params)
+
+       if @post.save
+           redirect_to @post
+       else
+           render 'new'
+       end
+   end
+
+   def show   
+       @post = Post.find(params[:id])   
+     end  
+
+
+
+   def edit
+    @post = Post.find(params[:id])
+end
+
+
+
+def update   
+    @post = Post.find(params[:id])   
+    if @post.update_attributes(post_params)   
+      flash[:notice] = 'Post updated!'   
+      redirect_to root_path   
+    else   
+      flash[:error] = 'Failed to edit post!'   
+      render :edit   
+    end   
+  end   
+
+   def destroy
+       @post = Post.find(params[:id])
+       @post.destroy
+
+       redirect_to posts_path
+
+   end
+
+   private
+
+   def post_params
+       params.require(:post).permit(:title, :content)
+   end
+
+   # def find_post
+   # 	@post = Post.find(params[:id])
+   # end
+
+end
